b1275f5baa46685898c49ce4835dded19797773c
Ray_Shangguan <a0963382999@gmail.com>
2022-09-05 17:34:07 +0800

    slightly update

--- main.py
+++ main.py
@@ -10,27 +10,32 @@ from alive_progress import alive_bar
 
 # sampleBit = input("input the sample bit of these audio data:\n")
 # soundLevel = input("input the loudness level you want:\n")
-
-examplePath = fr"C:\Users\{os.getlogin()}\Desktop\python"
-path = input(f"Please enter wav file location e.g. {examplePath} \n")
-roots = os.walk(path)
-resultFileName = input('enter the result xlsx file name e.g. Result_AMB.xlsx \n')
-amplitudeTester = ModelTester(model_path='keras_model.tflite',outputName=resultFileName)
-for parent, dirnames, filenames in roots:
-    for i in range(5):
-        print(f"round {i+1} start,increase decibel is {i*3} db!")
-        with alive_bar(len(filenames)) as bar:
-            for filename in filenames:
-                if filename[-3:] == "wav":
-                    filePath = path + '\\' + filename
-                    amplitudeTester.loadWavFile(filePath)
-                    # print(amplitudeTester.max_dBFS)
-                    amplitudeTester.NormalizedMaxDBFS(-20)
-                    amplitudeTester.volumeAdjustByAmp(i*3)
-                    amplitudeTester.doLibrosa()
-                bar()
-            amplitudeTester.exportDeatils()
-            # amplitudeTester.showResult()
-            amplitudeTester.exportResult()
-            amplitudeTester.reset()
-            
+isRepeat = True
+while isRepeat:
+    examplePath = fr"C:\Users\{os.getlogin()}\Desktop\python"
+    path = input(f"Please enter wav file location e.g. {examplePath} \n")
+    roots = os.walk(path)
+    # resultFileName = input('enter the result xlsx file name e.g. Result_AMB \n')
+    amplitudeTester = ModelTester(model_path='keras_model.tflite',outputName=path[-3:]+'.xlsx')
+    for parent, dirnames, filenames in roots:
+        for i in range(5):
+            print(f"round {i+1} start,increase decibel is {i*3} db!")
+            with alive_bar(len(filenames)) as bar:
+                for filename in filenames:
+                    if filename[-3:] == "wav":
+                        filePath = path + '\\' + filename
+                        amplitudeTester.loadWavFile(filePath)
+                        # print(amplitudeTester.max_dBFS)
+                        amplitudeTester.NormalizedMaxDBFS(-20)
+                        amplitudeTester.volumeAdjustByAmp(i*3)
+                        amplitudeTester.doLibrosa()
+                    bar()
+                amplitudeTester.exportDeatils()
+                # amplitudeTester.showResult()
+                amplitudeTester.exportResult()
+                amplitudeTester.reset()
+    repeat = input('if want to do again,please enter Y/y;if not,enter other key to leave\n')
+    if repeat == 'y' or repeat == 'Y':
+        isRepeat = True
+    else:
+        isRepeat = False
